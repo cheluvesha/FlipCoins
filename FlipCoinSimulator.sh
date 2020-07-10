c01dadff67a85b0f15e8a4428e8c4e4672066ebc
cheluvesha <cheluvesh.b@gmail.com>
2020-07-10 22:27:32 +0530

    Multime time Fliping coin

--- FlipCoinSimulator.sh
+++ FlipCoinSimulator.sh
@@ -1,9 +1,20 @@
 #!/bin/bash -x
+read -p "Enter the range to flip: " num
+val=1;
+count_head=0;
+count_tail=0;
+while [ $val -le $num ]
+do
 guess=$(( RANDOM% 2))
 if [ $guess -eq 1 ]
 then
-        echo "HEAD"
+        ((count_head++))
 else
-        echo "TAIL"
+        ((count_tail++))
 fi
+((val++))
+done
+echo "Number of times Heads won: "$count_head
+echo "Number of times Tails won: "$count_tail
+
 
