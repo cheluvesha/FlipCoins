ea4a35371eabf83eaa2444d952cd9cf27273d5f2
CHELUVESHA <38749165+cheluvesha@users.noreply.github.com>
2020-07-15 12:57:12 +0530

    Continuing a tie condition

--- FlipCoinSimulator.sh
+++ FlipCoinSimulator.sh
@@ -1,4 +1,33 @@
 #!/bin/bash
+#UC1
+guess=$(( RANDOM% 2))
+if [ $guess -eq 1 ]
+then
+        echo "HEAD"
+else
+        echo "TAIL"
+fi
+
+#UC2
+read -p "Enter the range to flip: " num
+val=1;
+count_head=0;
+count_tail=0;
+while [ $val -le $num ]
+do
+guess=$(( RANDOM% 2))
+if [ $guess -eq 1 ]
+then
+        ((count_head++))
+else
+        ((count_tail++))
+fi
+((val++))
+done
+echo "Number of times Heads won: "$count_head
+echo "Number of times Tails won: "$count_tail
+
+#UC3
 flag=1;
 Hcount=0;
 Lcount=0;
