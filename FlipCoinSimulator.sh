165a9d430deeb10ace380127964e12d8ac0a06a9
CHELUVESHA <38749165+cheluvesha@users.noreply.github.com>
2020-07-15 13:01:57 +0530

    Continuing the tie condition

--- FlipCoinSimulator.sh
+++ FlipCoinSimulator.sh
@@ -1,4 +1,63 @@
 #!/bin/bash 
+#UC1
+guess=$(( RANDOM% 2))
+if [ $guess -eq 1 ]
+then
+        echo "HEAD"
+else
+        echo "TAIL"
+fi
+
+#UC2
+read -p "Enter the range to flip: " num
+val=1;
+count_head=0;
+count_tail=0;
+while [ $val -le $num ]
+do
+guess=$(( RANDOM% 2))
+if [ $guess -eq 1 ]
+then
+        ((count_head++))
+else
+        ((count_tail++))
+fi
+((val++))
+done
+echo "Number of times Heads won: "$count_head
+echo "Number of times Tails won: "$count_tail
+
+#UC3
+flag=1;
+Hcount=0;
+Lcount=0;
+while [ $flag -eq 1 ]
+do
+guess=$(( RANDOM% 2))
+if [ $guess -eq 1 ]
+then
+        Hcount=`expr $Lcount + 1`
+else
+        Lcount=`expr $Lcount + 1`
+fi
+        if [[ $Hcount -eq 21 ]] || [[ $Lcount -eq 21 ]]
+        then
+        break;
+        else
+        continue;
+        fi
+done
+if [[ $Hcount -gt $Lcount ]]
+then
+        echo "Heads won this many times: "$Hcount
+elif [[ $Lcount -gt $Hcount ]]
+then
+        echo "Tails won this many times: "$Lcount
+else
+        echo "It's a tie between them "
+fi
+
+#UC4
 Hcount=21;
 Lcount=21;
 H_count=0;
@@ -25,9 +84,9 @@ done
 fi
 if [ $H_count -gt $L_count ]
         then
-                echo  "Heads Won with $H_count number of times"
+                echo  "Heads won"
         else
-                echo "Tails Won with $L_count number of times"
+                echo "Tails Won"
         fi
 
 
